93018a8f9a506bf294034c82d7c7d2ea80f6cbbe
Josua Krause <josua.krause@gmail.com>
2017-09-15 12:46:29 -0400

    fix address

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/env sh
 cd /projects/explanation_explorer/
 source venv/bin/activate
-python server.py output/examplemodel.csv output/examplemodel.lime.json
+python server.py --heroku -a 0 -p 15202 output/examplemodel.csv output/examplemodel.lime.json
